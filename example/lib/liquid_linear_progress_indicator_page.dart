d1789727a8ca7f713abeb5752763e245e1aac39a
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-05-15 23:19:08 +0530

    Dart 3.0 migrated

--- example/lib/liquid_linear_progress_indicator_page.dart
+++ example/lib/liquid_linear_progress_indicator_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
+import 'package:liquid_progress_indicator_ns/liquid_progress_indicator.dart';
 
 class LiquidLinearProgressIndicatorPage extends StatelessWidget {
   @override
